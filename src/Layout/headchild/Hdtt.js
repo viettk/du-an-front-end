9594a7e604e28f09c30709fb5ff3afe7e49ba962
unknown <viettkph09818@fpt.edu.vn>
2021-12-13 16:21:18 +0700

    ok

--- src/Layout/headchild/Hdtt.js
+++ src/Layout/headchild/Hdtt.js
@@ -15,14 +15,14 @@ function Hdtt(){
              <p>Hiện tại trên website của Bandai Store  chấp nhận phương thức thanh toán online và thanh toán khi nhận hàng.</p>
              <p><b className="text-hd">Hướng dẫn thanh toán online sau khi đặt hàng trên website:</b></p>
              <p>1</p>
-             <img className='img-chil' src={a1} style={{width:"auto", height:"auto", margin:"auto"}}/>
+             <img className='img-chil' style={{width:"auto", height:"auto", margin:"auto"}}/>
              <p>2</p>
              <div className="hinh-anh">
-             <img className='img-chil' src={a3} style={{width:"100%"}}/>
-             <img className='img-chil' src={a4} style={{width:"100%"}}/>
+             <img className='img-chil' style={{width:"100%"}}/>
+             <img className='img-chil' style={{width:"100%"}}/>
              </div>
              <p><b className="text-hd">Hình ảnh hướng dẫn tại trang thanh toán của website:</b></p>
-             <img className='img-chil' src={a2} style={{width:"auto", height:"auto", margin:"auto"}}/>
+             <img className='img-chil' style={{width:"auto", height:"auto", margin:"auto"}}/>
         </span>
         </div>
       </section>
